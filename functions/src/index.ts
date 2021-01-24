16a0f3432b1f26b47f044e68bb8c41c7be8b51af
Andresito de Guzman <andresitomyemail@gmail.com>
2021-01-25 01:30:56 +0800

    feat: update files

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,39 @@
+/**
+ * PWA Pilipinas
+ * 2020
+ * 
+ * All Rights Reserved
+ */
 import * as functions from 'firebase-functions';
+import * as express from 'express';
+import * as cors from 'cors';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const isbot = require('isbot')
+
+const appTitle = 'stest';
+
+const app = express();
+
+// Middlewares
+app.use(cors({ origin: true }));
+app.use((req: any, res: any, next: any) => {
+    if(isbot(req.get('User-Agent'))) {
+        // BOTS
+        app.get('/', (req, res) => {
+            res.json({ua: req.get('User-Agent')});
+        });
+        app.get('/id/:id', (req, res) => {
+            res.json({id: req.params.id, ua: req.get('User-Agent')});
+        });
+    }  else {
+        // @ts-ignore
+        app.use('/', express.static('src/dist'));
+    }
+    next();
+});
+
+
+
+
+
+exports[appTitle] = functions.https.onRequest(app);
