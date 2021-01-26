cd0245ccb01ac419f1b462ac381e2118612ce3a6
Andresito de Guzman <andresitomyemail@gmail.com>
2021-01-27 00:37:28 +0800

    feat: add prerender-node middleware

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,7 +8,7 @@ import * as functions from 'firebase-functions';
 import * as express from 'express';
 import * as cors from 'cors';
 
-const isbot = require('isbot')
+// const isbot = require('isbot')
 
 const appTitle = 'stest';
 
@@ -16,24 +16,9 @@ const app = express();
 
 // Middlewares
 app.use(cors({ origin: true }));
-app.use((req: any, res: any, next: any) => {
-    if(isbot(req.get('User-Agent'))) {
-        // BOTS
-        app.get('/', (req, res) => {
-            res.json({ua: req.get('User-Agent')});
-        });
-        app.get('/id/:id', (req, res) => {
-            res.json({id: req.params.id, ua: req.get('User-Agent')});
-        });
-    }  else {
-        // @ts-ignore
-        app.use('/', express.static('src/dist'));
-    }
-    next();
-});
+app.use(require('prerender-node'));
 
-
-
-
+//@ts-ignore
+app.use('/', express.static('src/dist'));
 
 exports[appTitle] = functions.https.onRequest(app);
